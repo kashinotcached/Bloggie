761309cf30db69d56f530caf96d76dbcace7db81
kashinotcached <kashinathnagvekar1@gmail.com>
2024-11-13 07:09:30 +0530

    property binding

--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,12 +1,20 @@
+using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Bloggie.Web.Pages.Admin.Blogs
+namespace Bloggie.Web.Pages.Admin.Blogs;
+
+public class AddModel : PageModel
 {
-    public class AddModel : PageModel
+    [BindProperty]
+    public AddBlogPost AddBlogPostRequest { get; set; }
+
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+    }
+
+    public void OnPost()
+    {
+
     }
 }
