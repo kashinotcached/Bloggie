721b8fceb82b388c3dd367835e96846eab8e169b
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-12 04:33:29 +0530

    Registered To Cloudinary and Settings

--- Bloggie.Web/Repositories/ImageRepositoryCloudinary.cs
+++ Bloggie.Web/Repositories/ImageRepositoryCloudinary.cs
@@ -1,10 +1,32 @@
 ﻿
+using CloudinaryDotNet;
+
 namespace Bloggie.Web.Repositories;
 
 public class ImageRepositoryCloudinary : IImageRepository
 {
-    public Task<string> UploadAsync(IFormFile file)
+    private readonly Account account;
+
+    public ImageRepositoryCloudinary(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        account = new Account(configuration.GetSection("Cloudinary")["CloudName"],
+            configuration.GetSection("Cloudinary")["ApiKey"],
+            configuration.GetSection("Cloudinary")["ApiSecret"]);
+    }
+
+    public async Task<string> UploadAsync(IFormFile file)
+    {
+        var client = new Cloudinary(account);
+        var uploadFileResult = await client.UploadAsync(
+            new CloudinaryDotNet.Actions.ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                DisplayName = file.FileName
+            });
+
+        if(uploadFileResult != null && uploadFileResult.StatusCode == System.Net.HttpStatusCode.OK)
+            return uploadFileResult.SecureUri.ToString();
+
+        return null;
     }
 }
