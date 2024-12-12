9b1cb73f306c53ec4e22c891514656a615bc044b
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-12 10:36:29 +0530

    Injected Image Repository and Tested Cloudinary

--- Bloggie.Web/Controllers/ImagesController.cs
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Repositories;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Bloggie.Web.Controllers;
 
@@ -6,9 +9,21 @@ namespace Bloggie.Web.Controllers;
 [Route("api/[controller]")]
 public class ImagesController : Controller
 {
+    private readonly IImageRepository imageRepository;
+
+    public ImagesController(IImageRepository imageRepository)
+    {
+        this.imageRepository = imageRepository;
+    }
+
     [HttpPost]
     public async Task<IActionResult> UploadAsync(IFormFile file)
     {
+        var imageUrl = await imageRepository.UploadAsync(file);
 
+        if(imageUrl == null)
+            return Problem("Something went wrong!", null, (int)HttpStatusCode.InternalServerError);
+
+        return Json(new { link = imageUrl });
     }
 }
