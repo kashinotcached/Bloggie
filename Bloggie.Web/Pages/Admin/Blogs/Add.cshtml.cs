b9b969ce24736ba5dd99b2d3b643829fb270a4e7
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-20 05:15:28 +0530

    Tag changes in add blogpost page

--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -17,6 +17,9 @@ public class AddModel : PageModel
     [BindProperty]
     public IFormFile FeaturedImage { get; set; }
 
+    [BindProperty]
+    public string Tags { get; set; }
+
     public AddModel(IBlogPostRepository blogPostRepository)
     {
         this.blogPostRepository = blogPostRepository;
@@ -39,7 +42,8 @@ public class AddModel : PageModel
             PublishedDate = AddBlogPostRequest.PublishedDate,
             ShortDescription = AddBlogPostRequest.ShortDescription,
             UrlHandle = AddBlogPostRequest.UrlHandle,
-            Visible = AddBlogPostRequest.Visible
+            Visible = AddBlogPostRequest.Visible,
+            Tags = new List<Tag>(Tags.Split(',').Select(x=> new Tag() { Name = x.Trim() }))
         };
 
         await blogPostRepository.AddAsync(blogPost);
