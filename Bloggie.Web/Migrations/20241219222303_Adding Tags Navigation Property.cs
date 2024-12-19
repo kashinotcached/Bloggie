8e7569e4278b60d7cc20f3c5643cd3178c2552cf
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-20 04:04:35 +0530

    DB changes --> Navigation Properties EF Core

---
+++ Bloggie.Web/Migrations/20241219222303_Adding Tags Navigation Property.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTagsNavigationProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TagId",
+                table: "Tags",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_TagId",
+                table: "Tags",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Tags_TagId",
+                table: "Tags",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Tags_TagId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_TagId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "Tags");
+        }
+    }
+}
--- Bloggie.Web/Models/Domain/Tag.cs
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -5,4 +5,7 @@ public class Tag
     public Guid Id { get; set; }
     public string Name { get; set; }
     public Guid BlogPostId { get; set; }
+
+    //Navigation Property
+    public ICollection<Tag> Tags { get; set; }
 }
