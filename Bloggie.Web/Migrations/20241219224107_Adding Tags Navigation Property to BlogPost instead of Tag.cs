dd85df25a52ba7dbe1b53a4044915228f20dc446
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-20 04:12:47 +0530

    fixed migration for navigation properties EF Core

---
+++ Bloggie.Web/Migrations/20241219224107_Adding Tags Navigation Property to BlogPost instead of Tag.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTagsNavigationPropertytoBlogPostinsteadofTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Tags_TagId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_TagId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "TagId",
+                table: "Tags");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_BlogPostId",
+                table: "Tags",
+                column: "BlogPostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_BlogPosts_BlogPostId",
+                table: "Tags",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_BlogPosts_BlogPostId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_BlogPostId",
+                table: "Tags");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TagId",
+                table: "Tags",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_TagId",
+                table: "Tags",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Tags_TagId",
+                table: "Tags",
+                column: "TagId",
+                principalTable: "Tags",
+                principalColumn: "Id");
+        }
+    }
+}
--- Bloggie.Web/Models/Domain/Tag.cs
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -5,7 +5,4 @@ public class Tag
     public Guid Id { get; set; }
     public string Name { get; set; }
     public Guid BlogPostId { get; set; }
-
-    //Navigation Property
-    public ICollection<Tag> Tags { get; set; }
 }
