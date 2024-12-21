c404f67ae14c2c5a13e0da66439ad1c2fad6d161
kashinotcached <kashinathnagvekar1@gmail.com>
2024-12-21 06:25:31 +0530

    Displayed tags on index page

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<BloggieDbContext>(options =>
 
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepositoryCloudinary>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
 
 var app = builder.Build();
 
