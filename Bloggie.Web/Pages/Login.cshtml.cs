db10247d7caf431ac8d061f438b3a73709251ffb
kashinotcached <kashinathnagvekar1@gmail.com>
2025-01-09 15:50:37 +0530

    Validations for Login Page

--- Bloggie.Web/Pages/Login.cshtml.cs
+++ Bloggie.Web/Pages/Login.cshtml.cs
@@ -23,26 +23,33 @@ public class LoginModel : PageModel
 
     public async Task<IActionResult> OnPost(string ReturnUrl)
     {
-        var signInResult = await signInManager.PasswordSignInAsync(
+        if (ModelState.IsValid)
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(
             LoginViewModel.Username, LoginViewModel.Password, false, false);
 
-        if (signInResult.Succeeded)
-        {
-            if (!string.IsNullOrWhiteSpace(ReturnUrl))
+            if (signInResult.Succeeded)
             {
-                return RedirectToPage(ReturnUrl);
-            }
+                if (!string.IsNullOrWhiteSpace(ReturnUrl))
+                {
+                    return RedirectToPage(ReturnUrl);
+                }
 
-            return RedirectToPage("Index");
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Error,
+                    Message = "Unable to login"
+                };
+
+                return Page();
+            }
         }
         else
         {
-            ViewData["Notification"] = new Notification
-            {
-                Type = Enums.NotificationType.Error,
-                Message = "Unable to login"
-            };
-
             return Page();
         }
     }
