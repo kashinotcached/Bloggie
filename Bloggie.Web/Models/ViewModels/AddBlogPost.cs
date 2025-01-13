fe30b16a805e78c37bca4ba31e1929c6651937aa
kashinotcached <kashinathnagvekar1@gmail.com>
2025-01-13 11:21:10 +0530

    validations and custom validations for Add Blog Post Page

--- Bloggie.Web/Models/ViewModels/AddBlogPost.cs
+++ Bloggie.Web/Models/ViewModels/AddBlogPost.cs
@@ -1,16 +1,35 @@
-﻿namespace Bloggie.Web.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bloggie.Web.Models.ViewModels
 {
     public class AddBlogPost
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string Heading { get; set; }
+
+        [Required]
         public string PageTitle { get; set; }
+
+        [Required]
         public string Content { get; set; }
+
+        [Required]
         public string ShortDescription { get; set; }
+
+        [Required]
         public string FeatureImageUrl { get; set; }
+
+        [Required]
         public string UrlHandle { get; set; }
+
+        [Required]
         public DateTime PublishedDate { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
         public bool Visible { get; set; }
     }
 }
--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -4,6 +4,7 @@ using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace Bloggie.Web.Pages.Admin.Blogs;
@@ -20,6 +21,7 @@ public class AddModel : PageModel
     public IFormFile FeaturedImage { get; set; }
 
     [BindProperty]
+    [Required]
     public string Tags { get; set; }
 
     public AddModel(IBlogPostRepository blogPostRepository)
@@ -33,31 +35,47 @@ public class AddModel : PageModel
 
     public async Task<IActionResult> OnPost()
     {
-        var blogPost = new BlogPost()
+        ValidateAddBlogPost();
+
+        if (ModelState.IsValid)
         {
-            Heading = AddBlogPostRequest.Heading,
-            Author = AddBlogPostRequest.Author,
-            Content = AddBlogPostRequest.Content,
-            FeatureImageUrl = AddBlogPostRequest.FeatureImageUrl,
-            Id = AddBlogPostRequest.Id,
-            PageTitle = AddBlogPostRequest.PageTitle,
-            PublishedDate = AddBlogPostRequest.PublishedDate,
-            ShortDescription = AddBlogPostRequest.ShortDescription,
-            UrlHandle = AddBlogPostRequest.UrlHandle,
-            Visible = AddBlogPostRequest.Visible,
-            Tags = new List<Tag>(Tags.Split(',').Select(x=> new Tag() { Name = x.Trim() }))
-        };
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                Author = AddBlogPostRequest.Author,
+                Content = AddBlogPostRequest.Content,
+                FeatureImageUrl = AddBlogPostRequest.FeatureImageUrl,
+                Id = AddBlogPostRequest.Id,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                Visible = AddBlogPostRequest.Visible,
+                Tags = new List<Tag>(Tags.Split(',').Select(x => new Tag() { Name = x.Trim() }))
+            };
 
-        await blogPostRepository.AddAsync(blogPost);
+            await blogPostRepository.AddAsync(blogPost);
 
-        var notification = new Notification
+            var notification = new Notification
+            {
+                Message = "New blog created!",
+                Type = Enums.NotificationType.Success
+            };
+
+            TempData["Notification"] = JsonSerializer.Serialize(notification);
+            
+            return RedirectToPage("List");
+        }
+
+        return Page();
+    }
+
+    private void ValidateAddBlogPost()
+    {
+        if(AddBlogPostRequest.PublishedDate.Date < DateTime.Now.Date)
         {
-            Message = "New blog created!",
-            Type = Enums.NotificationType.Success
-        };
-
-        TempData["Notification"] = JsonSerializer.Serialize(notification);
-
-        return RedirectToPage("List");
+            ModelState.AddModelError("AddBlogPostRequest.PublishedDate", 
+                $"{nameof(AddBlogPostRequest.PublishedDate)} can only be today's date or a future date.");
+        }
     }
 }
